0c031a0cb611f85f91af8c014fa891332c54fe73
PedroAlexandreFS <pedroalexandrea13@gmail.com>
2026-08-21 00:22:43 -0300

    Primeira atividade de js

---
+++ Fundamentos/TiposDeSaida.js
@@ -0,0 +1,8 @@
+document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS<b>!";
+
+//---------   USADO PARA TESTE   -----------
+document.write("Oi <u>Escrito com document.write</u>");
+
+alert("isso é um alerta");
+
+console.log("olá mundo");
